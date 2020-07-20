d02f223151ad209a2db93baf3ee52dcb39ed5dd9
shantanuatgit <shantanu3250@.com>
2020-07-20 09:57:41 +0530

    removed secret key for security purposes

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ka47l7q&v_m41$enjxxtscka&@@$6b%f+6^vlwn$0sxi1ad#+g'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,3 +128,7 @@ STATICFILES_DIRS=[
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL= '/media/'
